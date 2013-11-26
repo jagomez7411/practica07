5522475a2c01cd29bb4a0243d9bca92302f274be
AGomez <AGomez>
2013-11-26 16:57:42 -0600

    sub6

--- js/acciones.js
+++ js/acciones.js
@@ -19,11 +19,12 @@ $(document).ready(function(e){
     //
     function Comienza() {
 
-        // Actualiza la aceleracion cada 3 segundos
+        // Actualiza la aceleracion cada 2 segundos
        //
         var opciones = { frequency: 2000 };
 
         watchID = navigator.accelerometer.watchAcceleration(Correcto, Error, opciones);
+		navigator.geolocation.getCurrentPosition(Localiza, ErrorLocalizacion);
     }
 
     // Detiene la 'observacion' de la aceleracion 
@@ -51,6 +52,24 @@ $(document).ready(function(e){
     function Error() {
         alert('Error!');
     }
+  // Exito al localizar
 
+    function Localiza(posicion) {
+        var element = document.getElementById('geolocalizacion');
+        element.innerHTML = 'Latitud: '          + posicion.coords.latitude         + '<br />' +
+                            'Longitud: '         + posicion.coords.longitude        + '<br />' +
+                            'Altitud: '          + posicion.coords.altitude         + '<br />' +
+                            'Precision: '          + posicion.coords.accuracy         + '<br />' +
+                            'Precision de Altitud: ' + posicion.coords.altitudeAccuracy + '<br />' +
+                            'Direccion: '           + posicion.coords.heading          + '<br />' +
+                            'Velocidad: '             + posicion.coords.speed            + '<br />' +
+                            'Intervalo: '         + posicion.timestamp               + '<br />';
+    }
 
+        // Error en la geolocalizacion
+        //
+        function ErrorLocalizacion(error) {
+            alert('codigo: '    + error.code    + '\n' +
+                  'mensaje: ' + error.message + '\n');
+		}
 });//documento ready
